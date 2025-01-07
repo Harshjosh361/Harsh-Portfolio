d21abf7ddd3290f8e2975ea425dbf37d2c639457
Harshjosh361 <harshjosh361@gmail.com>
2025-01-07 12:42:10 +0530

    update footer to display the current year dynamically

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className='border-b border-neutral-900  text-white flex flex-col justify-center items-center p-4 m-2'>
-        <p>Copyright © 2024 - All rights reserved</p>
+        <p>Copyright © {currentYear} - All rights reserved</p>
         <p>Harsh Joshi</p>
     </div>
   )
